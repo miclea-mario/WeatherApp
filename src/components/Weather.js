0d235ac00bc693cc643afbceb63391a940341f4c
Mario Miclea <miclea.alexandru.mario@gmail.com>
2024-01-23 23:51:18 +0200

    Small fixes

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Input, Card, CardBody, Button, Divider } from "@nextui-org/react";
+import { Input, Card, CardBody, Button, Divider, Chip } from "@nextui-org/react";
 import UVIndexCard from "./UVIndexCard";
 import TextInfoCard from "./TextInfoCard";
 import WindCard from "./WindCard";
@@ -72,119 +72,124 @@ export default function Weather(props) {
     });
 
   return (
-    <Card
-      isBlurred
-      className="border-none bg-default-100/10 md:bg-default-100/50 px-4 md:px-0 md:w-[1200px] md:min-w-[900px]"
-      shadow="lg"
-    >
-      <CardBody className="p-1 md:p-0">
-        <div className="md:flex items-center grow">
-          <div className="grid gap-2 bg-white/25 p-4 w-full md:w-3/12 rounded-b-2xl md:rounded-large">
-            <div className="flex items-center gap-4">
-              <Input
-                label="Search"
-                isClearable
-                radius="lg"
-                value={inputValue}
-                onClear={() => setInputValue("")}
-                onChange={(e) => {
-                  setInputValue(e.target.value);
-                }}
-                classNames={{
-                  label: "text-black/50 dark:text-white/90",
-                  input: [
-                    "bg-transparent",
-                    "text-black/90 dark:text-white/90",
-                    "placeholder:text-slate-700/50",
-                  ],
-                  innerWrapper: "bg-transparent",
-                  inputWrapper: [
-                    "shadow-md",
-                    "bg-default-200/10",
-                    "backdrop-saturate-200",
-                    "hover:bg-default-200/70",
-                    "group-data-[focused=true]:bg-default-200/50",
-                    "!cursor-text",
-                  ],
-                }}
-                placeholder="Type to search..."
-                startContent={
-                  <LocationIcon className="text-slate-700/50 mb-0.5 text-slate-700 pointer-events-none flex-shrink-0" />
-                }
-              />
-              <Button
-                isIconOnly
-                color="primary"
-                onPress={() => {
-                  props.onSearchChange(inputValue);
-                }}
-              >
-                <SearchIcon />
-              </Button>
-            </div>
+    <div className="flex flex-col items-center">
+      <Card
+        isBlurred
+        className="border-none bg-default-100/10 md:bg-default-100/50 px-4 md:px-0 md:w-[1200px] md:min-w-[900px]"
+        shadow="lg"
+      >
+        <CardBody className="p-1 md:p-0">
+          <div className="md:flex items-center grow">
+            <div className="grid gap-2 bg-white/25 p-4 w-full md:w-3/12 rounded-b-2xl md:rounded-large">
+              <div className="flex items-center gap-4">
+                <Input
+                  label="Search"
+                  isClearable
+                  radius="lg"
+                  value={inputValue}
+                  onClear={() => setInputValue("")}
+                  onChange={(e) => {
+                    setInputValue(e.target.value);
+                  }}
+                  classNames={{
+                    label: "text-black/50 dark:text-white/90",
+                    input: [
+                      "bg-transparent",
+                      "text-black/90 dark:text-white/90",
+                      "placeholder:text-slate-700/50",
+                    ],
+                    innerWrapper: "bg-transparent",
+                    inputWrapper: [
+                      "shadow-md",
+                      "bg-default-200/10",
+                      "backdrop-saturate-200",
+                      "hover:bg-default-200/70",
+                      "group-data-[focused=true]:bg-default-200/50",
+                      "!cursor-text",
+                    ],
+                  }}
+                  placeholder="Type to search..."
+                  startContent={
+                    <LocationIcon className="text-slate-700/50 mb-0.5 text-slate-700 pointer-events-none flex-shrink-0" />
+                  }
+                />
+                <Button
+                  isIconOnly
+                  color="primary"
+                  onPress={() => {
+                    props.onSearchChange(inputValue);
+                  }}
+                >
+                  <SearchIcon />
+                </Button>
+              </div>
 
-            <h1 className="text-lg">
-              {props.weatherData.resolvedAddress || "Getting location..."}
-            </h1>
-            {getConditionIcon(props.weatherData.currentConditions.icon)}
-            <h2 className="text-6xl font-thin">
-              {toFahrenheit(props.weatherData.currentConditions.temp)}
-              <span className="text-xl align-super">{props.unit}</span>
-            </h2>
-            <p>{props.weatherData.currentConditions.conditions}</p>
-            <Divider className="my-4" />
-            <p>{props.date}</p>
-          </div>
-          <div className="w-full md:w-9/12 p-4 flex flex-col gap-4">
-            <div>
-              <Button isIconOnly onPress={() => props.onUnitChange()}>
-                {props.unit}
-              </Button>
+              <h1 className="text-lg">
+                {props.weatherData.resolvedAddress || "Getting location..."}
+              </h1>
+              {getConditionIcon(props.weatherData.currentConditions.icon)}
+              <h2 className="text-6xl font-thin">
+                {toFahrenheit(props.weatherData.currentConditions.temp)}
+                <span className="text-xl align-super">{props.unit}</span>
+              </h2>
+              <p>{props.weatherData.currentConditions.conditions}</p>
+              <Divider className="my-4" />
+              <p>{props.date}</p>
             </div>
-            <div className="flex flex-row overflow-x-auto md:grid-cols-7 gap-4 scrollbar-hide justify-between">
-              {dayCards}
-            </div>
+            <div className="w-full md:w-9/12 p-4 flex flex-col gap-4">
+              <div>
+                <Button isIconOnly onPress={() => props.onUnitChange()}>
+                  {props.unit}
+                </Button>
+              </div>
+              <div className="flex flex-row overflow-x-auto md:grid-cols-7 gap-4 scrollbar-hide justify-between">
+                {dayCards}
+              </div>
 
-            <div className="grid grid-cols-2 md:grid-cols-5 gap-4">
-              <UVIndexCard
-                indexLevel={props.weatherData.currentConditions.uvindex}
-              />
-              <TextInfoCard
-                Icon={DropletIcon}
-                category="Rainfall"
-                value={
-                  props.weatherData.currentConditions.precip
-                    ? `${props.weatherData.currentConditions.precip.toFixed(1)}`
-                    : `0`
-                }
-                unit=" mm"
-                subtitle="in last hour"
-                bottomText={`${props.weatherData.days[1].precip} mm expected in next 24h.`}
-              />
-              <TextInfoCard
-                Icon={TemperatureIcon}
-                category="Feels Like"
-                value={`${toFahrenheit(
-                  props.weatherData.currentConditions.feelslike
-                )}`}
-                unit={`${props.unit}`}
-                bottomText={props.weatherData.description}
-              />
-              <TextInfoCard
-                Icon={HumidityIcon}
-                category="Humidity"
-                value={`${props.weatherData.currentConditions.humidity}`}
-                unit="%"
-                bottomText={`The dew point is ${props.weatherData.currentConditions.dew} right now.`}
-              />
-              <WindCard
-                windSpeed={props.weatherData.currentConditions.windspeed}
-                windDirection={props.weatherData.currentConditions.winddir}
-              />
+              <div className="grid grid-cols-2 md:grid-cols-5 gap-4">
+                <UVIndexCard
+                  indexLevel={props.weatherData.currentConditions.uvindex}
+                />
+                <TextInfoCard
+                  Icon={DropletIcon}
+                  category="Rainfall"
+                  value={
+                    props.weatherData.currentConditions.precip
+                      ? `${props.weatherData.currentConditions.precip.toFixed(
+                          1
+                        )}`
+                      : `0`
+                  }
+                  unit=" mm"
+                  subtitle="in last hour"
+                  bottomText={`${props.weatherData.days[1].precip} mm expected in next 24h.`}
+                />
+                <TextInfoCard
+                  Icon={TemperatureIcon}
+                  category="Feels Like"
+                  value={`${toFahrenheit(
+                    props.weatherData.currentConditions.feelslike
+                  )}`}
+                  unit={`${props.unit}`}
+                  bottomText={props.weatherData.description}
+                />
+                <TextInfoCard
+                  Icon={HumidityIcon}
+                  category="Humidity"
+                  value={`${props.weatherData.currentConditions.humidity}`}
+                  unit="%"
+                  bottomText={`The dew point is ${props.weatherData.currentConditions.dew} right now.`}
+                />
+                <WindCard
+                  windSpeed={props.weatherData.currentConditions.windspeed}
+                  windDirection={props.weatherData.currentConditions.winddir}
+                />
+              </div>
             </div>
           </div>
-        </div>
-      </CardBody>
-    </Card>
+        </CardBody>
+      </Card>
+      <Chip  className="mt-8 w-fit bg-white/80 backdrop-blur-sm" size="sm">made by Mario Miclea</Chip>
+    </div>
   );
 }
